df1e7624dac3a18d1a5576623d55d8cba0ffef42
hyungkishin <tkaqkeldk@nate.com>
2024-07-20 23:25:42 +0900

    chore: initial commit

---
+++ src/test/java/com/example/springbatchcsv/SpringBatchCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchcsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchCsvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
